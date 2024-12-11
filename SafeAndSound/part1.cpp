9659a9742b04fb6a2254ba568c04e1bab97bb472
Liroyyo <145814568+Liroyyo@users.noreply.github.com>
2024-12-11 19:21:47 +0200

    Add files via upload

---
+++ SafeAndSound/part1.cpp
@@ -0,0 +1,25 @@
+#include "part1.h"
+#include <iostream>
+
+char* string_copy(char dest[], int destsize, char* src)
+{
+	char* ret = dest;
+	int i = 0;
+	while (i < destsize - 1 && (*dest++ = *src++)) {
+		i++;
+	}
+	*dest = '\0';
+	return ret;
+}
+
+void part1()
+{
+	char password[] = "secret";
+	char dest[13];
+	char src[] = "hello world!";
+
+	string_copy(dest, 13, src);
+
+	std::cout << src << std::endl;
+	std::cout << dest << std::endl;
+}
